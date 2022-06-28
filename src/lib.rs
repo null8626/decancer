28d58a57dab64a7be9c22a02b17c5e8ebc0b9d60
null8626 <vierofernando9@gmail.com>
2022-06-28 14:20:07 +0700

    feat: filter out control characters [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,7 @@ fn decancer(raw_input: JsString) -> Result<String, Error> {
 
   // for_each so we can have return (implement some sort of goto in rust)
   Codepoints::from(&input)
-    .filter(|&x| x != 0x20E3 && x != 0xFE0F && (x < 0x300 || x > 0x36F) && x != 0x489)
+    .filter(|&x| ((x > 31 && x < 127) || (x > 159 && x < 0x300) || x > 0x36F) && x != 0x20E3 && x != 0xFE0F && x != 0x489)
     .for_each(|x| {
       for num in confusables::numerical() {
         if x >= num && x <= (num + 9) {
@@ -104,9 +104,9 @@ fn decancer(raw_input: JsString) -> Result<String, Error> {
   output.retain(|c2| {
     let (a, b) = charcodes(c2 as _);
 
-    if a != 0 && a != 0xFFFD && (a < 0xD800 || a > 0xDB7F) && a < 0xFFF0 {
+    if a != 0xFFFD && (a < 0xD800 || a > 0xDB7F) && a < 0xFFF0 {
       if let Some(b2) = b {
-        b2 != 0 && b2 != 0xFFFD && (b2 < 0xD800 || b2 > 0xDB7F) && b2 < 0xFFF0
+        b2 != 0xFFFD && (b2 < 0xD800 || b2 > 0xDB7F) && b2 < 0xFFF0
       } else {
         true
       }
