78205c94bb4565a1d42637db7c26a4d9d335efe7
null8626 <vierofernando9@gmail.com>
2022-07-30 23:46:59 +0700

    refactor: fix ci bug

--- node/src/lib.ts
+++ node/src/lib.ts
@@ -1,8 +1,10 @@
 import assert from 'node:assert'
 import { existsSync, readFileSync } from 'node:fs'
-import { join } from 'node:path'
-
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import type Decancer from './typings'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 type Option<T> = T | undefined | null
 type Arch =
@@ -32,7 +34,7 @@ function isMusl(): boolean {
 }
 
 function loadBinding(name: string) {
-  const path: string = join(__dirname, '..', `decancer.${name}.node`)
+  const path: string = resolve(__dirname, '..', `decancer.${name}.node`)
 
   // @ts-ignore: this will NOT be null :)
   let exported: Decancer = null
