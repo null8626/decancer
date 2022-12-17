5e9ecd23547ff3d45b1a62e8a3b6ad311949e333
null8626 <vierofernando9@gmail.com>
2022-12-17 08:22:52 +0700

    feat: add proper functions [skip ci]

--- core/src/similar.rs
+++ core/src/similar.rs
@@ -3,58 +3,36 @@ use super::{matcher::CONFUSABLES, util::read_u16_le};
 const SIMILAR_START: u16 = read_u16_le(CONFUSABLES);
 const SIMILAR_END: u16 = read_u16_le(unsafe { CONFUSABLES.offset(2) });
 
-pub(crate) fn is(a: *const u8, b: *const u8) -> u8 {
-  unsafe {
-    if *a == *b {
-      if *a <= 0x7F {
-        1
-      } else {
-        let count: u8 = {
-          if *a >= 0xF0 {
-            4
-          } else if *a >= 0xE0 {
-            3
-          } else {
-            2
-          }
-        };
+pub(crate) fn is(a: u32, b: u32) -> bool {
+  if a > 0xff || b > 0xff {
+    a == b
+  } else if a == b {
+    true
+  } else {
+    let mut offset = SIMILAR_START;
 
-        for i in 1isize..count as isize {
-          if *a.offset(i) != *b.offset(i) {
-            return 0;
-          }
+    while offset < SIMILAR_END {
+      let mut contains_a = false;
+      let mut contains_b = false;
+
+      let len = unsafe { *CONFUSABLES.offset(offset as _) };
+      offset += 1;
+
+      for part in (offset..offset + (len as u16)).map(|x| unsafe { *CONFUSABLES.offset(x as _) }) {
+        if part == (a as u8) {
+          contains_a = true;
+        } else if part == (b as u8) {
+          contains_b = true;
         }
 
-        count
-      }
-    } else if *b > 0x7F || *a > 0x7F {
-      0 // nah fuck it we're giving up this time
-    } else {
-      let mut offset = SIMILAR_START;
-
-      while offset < SIMILAR_END {
-        let mut contains_a = false;
-        let mut contains_b = false;
-
-        let len = *CONFUSABLES.offset(offset as _);
-        offset += 1;
-
-        for part in (offset..offset + (len as u16)).map(|x| *CONFUSABLES.offset(x as _)) {
-          if part == *a {
-            contains_a = true;
-          } else if part == *b {
-            contains_b = true;
-          }
-
-          if contains_a && contains_b {
-            return 1;
-          }
+        if contains_a && contains_b {
+          return true;
         }
-
-        offset += len as u16;
       }
 
-      0
+      offset += len as u16;
     }
+
+    false
   }
 }
--- core/src/string.rs
+++ core/src/string.rs
@@ -30,63 +30,80 @@ impl CuredString {
     self.0.push(unsafe { transmute(code) })
   }
 
-  #[inline(always)]
-  pub fn into_str(self) -> String {
-    self.0
+  pub const fn into_str(self) -> String {
+    unsafe { transmute(self) }
   }
 
   pub fn starts_with<S: AsRef<str> + ?Sized>(&self, other: &S) -> bool {
     let o = other.as_ref();
 
-    if self.len() < o.len() || self.is_empty() {
-      false
-    } else {
-      unsafe {
-        let mut s_ptr = self.as_ptr();
-        let mut o_ptr = o.as_ptr();
-        let end = o_ptr.offset(o.len() as _);
+    if o.len() > self.len() {
+      return false;
+    }
 
-        while o_ptr < end {
-          let offset = similar::is(s_ptr, o_ptr);
+    let mut other_iter = o.chars();
 
-          if offset == 0 {
+    for self_char in self.chars() {
+      match other_iter.next() {
+        Some(other_char) => {
+          if !similar::is(self_char as _, other_char as _) {
             return false;
-          } else {
-            s_ptr = s_ptr.offset(offset as _);
-            o_ptr = o_ptr.offset(offset as _);
           }
         }
 
-        true
-      }
+        None => return true,
+      };
     }
+
+    false
   }
 
   pub fn ends_with<S: AsRef<str> + ?Sized>(&self, other: &S) -> bool {
     let o = other.as_ref();
 
-    if self.len() < o.len() || self.is_empty() {
-      false
-    } else {
-      unsafe {
-        let mut s_ptr = self.as_ptr().offset((self.len() - 1) as _);
-        let mut o_ptr = o.as_ptr().offset((o.len() - 1) as _);
+    if o.len() > self.len() {
+      return false;
+    }
 
-        while o_ptr >= o.as_ptr() {
-          compute_reversed_utf8(&mut s_ptr);
-          compute_reversed_utf8(&mut o_ptr);
+    let mut other_iter = o.chars().rev();
 
-          if similar::is(s_ptr, o_ptr) == 0 {
+    for self_char in self.chars().rev() {
+      match other_iter.next() {
+        Some(other_char) => {
+          if !similar::is(self_char as _, other_char as _) {
             return false;
-          } else {
-            s_ptr = s_ptr.offset(-1);
-            o_ptr = o_ptr.offset(-1);
           }
         }
 
-        true
+        None => return true,
+      };
+    }
+
+    false
+  }
+
+  pub fn contains<S: AsRef<str> + ?Sized>(&self, other: &S) -> bool {
+    let o = other.as_ref();
+    if o.len() > self.len() {
+      return false;
+    }
+
+    let other_chars = o.chars().collect::<Vec<_>>();
+    let mut other_index = 0usize;
+
+    for self_char in self.chars() {
+      if similar::is(other_chars[other_index] as _, self_char as _) {
+        other_index += 1;
+
+        if other_index == other_chars.len() {
+          return true;
+        }
+      } else {
+        other_index = 0;
       }
     }
+
+    false
   }
 }
 
@@ -114,24 +131,21 @@ where
     if self.len() != o.len() {
       false
     } else {
-      unsafe {
-        let mut s_ptr = self.as_ptr();
-        let mut o_ptr = o.as_ptr();
-        let end = s_ptr.offset(self.len() as _);
+      let mut other_iter = o.chars();
 
-        while s_ptr < end {
-          let offset = similar::is(s_ptr, o_ptr);
-
-          if offset == 0 {
-            return false;
-          } else {
-            s_ptr = s_ptr.offset(offset as _);
-            o_ptr = o_ptr.offset(offset as _);
+      for self_char in self.chars() {
+        match other_iter.next() {
+          Some(other_char) => {
+            if !similar::is(self_char as _, other_char as _) {
+              return false;
+            }
           }
-        }
 
-        true
+          None => return false,
+        };
       }
+
+      true
     }
   }
 }
