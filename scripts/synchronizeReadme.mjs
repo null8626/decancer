b95ed983e592418c9834388d69eb5a7492dbf6c5
null8626 <vierofernando9@gmail.com>
2022-12-18 23:33:52 +0700

    ci: attempt 7 (depression)

--- scripts/synchronizeReadme.mjs
+++ scripts/synchronizeReadme.mjs
@@ -1,9 +1,9 @@
 import { execSync } from 'node:child_process'
 import { copyFileSync } from 'node:fs'
 
-let file = execSync('git diff --name-only -- HEAD HEAD~1').toString().trim().split(/\r?\n/)
+let file = execSync('git diff --name-only -- HEAD HEAD~1').toString().trim()
 console.log(file)
-file = file.find(x => x.endsWith('README.md'))
+file = file.split('\n').find(x => x.endsWith('README.md'))
 
 const copiedTo = file === 'README.md' ? './node/README.md' : './README.md'
 
