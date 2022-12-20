bf8275d4af631a065c179c5cab7ece000ed8ffe0
null8626 <vierofernando9@gmail.com>
2022-12-20 17:37:41 +0700

    *: ci rewrite [skip ci]

---
+++ scripts/version.mjs
@@ -0,0 +1,29 @@
+import { readFileSync, writeFileSync, readdirSync } from 'node:fs'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const ROOT_DIR = join(dirname(fileURLToPath(import.meta.url)), '..')
+const update = (filename, callback) => {
+  writeFileSync(filename, callback(readFileSync(filename).toString()))
+  console.log(`- updated file: ${filename}`)
+}
+
+const updateTomlFunc = x => x.replace(/version = "\d\.\d\.\d"/, `version = "${process.argv[2]}"`)
+
+update(join(ROOT_DIR, 'core', 'Cargo.toml'), updateTomlFunc)
+update(join(ROOT_DIR, 'wasm', 'Cargo.toml'), updateTomlFunc)
+
+const updateJsonFunc = x => {
+  const json = JSON.parse(x)
+  json.version = process.argv[2]
+  
+  return JSON.stringify(json, null, 2)
+}
+
+update(join(ROOT_DIR, 'node', 'package.json'), updateJsonFunc)
+
+for (const dir of readdirSync(join(ROOT_DIR, 'node', 'npm'))) {
+  update(join(ROOT_DIR, 'node', 'npm', dir, 'package.json'), updateJsonFunc)
+}
+
+update(join(ROOT_DIR, 'README.md'), x => x.replace(/(\d\.\d\.\d)/g, process.argv[2]))
