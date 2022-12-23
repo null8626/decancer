c6dde0456358b83f4aa4aff1112b70a68faf1f79
null8626 <vierofernando9@gmail.com>
2022-12-23 17:19:37 +0700

    1.5.0

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,16 +1,16 @@
 //! # decancer
-//! 
+//!
 //! A tiny package that removes common confusables from strings.
-//! 
+//!
 //! Pros:
-//! 
+//!
 //! - BLAZINGLY FAST™ 🚀🚀🚀, no use of regex whatsoever!
 //! - No use of any external dependencies.
 //! - Very simple to use!
 //! - Supports more than **3000 unicode codepoints**. This should cover the vast majority of confusables, including emojis, zalgos, etc.
-//! 
+//!
 //! Con:
-//! 
+//!
 //! - Remember that this project is not perfect, false-positives may happen.
 
 #![allow(
