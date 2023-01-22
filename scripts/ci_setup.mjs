92e767577dfe576c78012e1ce0a4dbca50db5c4f
null8626 <vierofernando9@gmail.com>
2023-01-22 13:22:55 +0700

    ci: refactor ci (attempt 15)

--- scripts/ci_setup.mjs
+++ scripts/ci_setup.mjs
@@ -1,48 +1,33 @@
-import { exec } from 'node:child_process'
 import { appendFileSync } from 'node:fs'
 import { EOL } from 'node:os'
 
-const execute = (command, cwd) =>
-  new Promise((resolve, reject) =>
-    exec(command, { cwd }, (error, stderr, stdout) =>
-      error ? reject(error?.stack) : resolve(stdout.toString().trim())
-    )
-  )
+const response = await fetch('https://api.github.com/repos/null8626/decancer/compare/HEAD~1...HEAD', {
+  headers: {
+	Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+	'Content-Type': 'application/json'
+  }
+})
 
-const [commit, filesChanged] = await Promise.all([
-  execute('git log -1 --pretty=%B'),
-  new Promise((resolve) =>
-    execute('git diff --name-only HEAD~1 HEAD').then((out) =>
-      resolve(out.split(EOL))
-    )
-  )
-])
-
-console.log(`commit: ${commit}`)
-console.log(`commit 2: ${filesChanged}`)
-
-const coreAffected = filesChanged.some(
-  (file) => file.startsWith('core/src/') || file === 'core/bin/confusables.bin'
-)
+const { base_commit: { message }, files } = await response.json()
 
 console.log(
   Object.entries({
-    is_release: /^\d+\.\d+\.\d+$/.test(commit),
+    is_release: /^\d+\.\d+\.\d+$/.test(message),
     core_affected: coreAffected,
     node_affected:
       coreAffected ||
-      filesChanged.some((file) => file.startsWith('bindings/node/src')),
+      files.some(({ filename }) => filename.startsWith('bindings/node/src')),
     wasm_affected:
       coreAffected ||
-      filesChanged.some((file) => file.startsWith('bindings/wasm/src')),
+      files.some(({ filename }) => filename.startsWith('bindings/wasm/src')),
     native_affected:
       coreAffected ||
       filesChanged.some(
-        (file) =>
-          file.startsWith('bindings/native/src') ||
-          file === 'bindings/native/decancer.h'
+        ({ filename }) =>
+          filename.startsWith('bindings/native/src') ||
+          filename === 'bindings/native/decancer.h'
       ),
-    readme_affected: filesChanged.includes('README.md')
+    readme_affected: filename.some(({ filename }) => filename === 'README.md')
   }).reduce((a, [k, v]) => `${a}${k}=${v}${EOL}`, '')
 )
 
