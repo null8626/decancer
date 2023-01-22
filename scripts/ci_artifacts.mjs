c2349113511097b31301928ed2ba2dbb7eada16a
null8626 <vierofernando9@gmail.com>
2023-01-22 21:14:51 +0700

    ci: native compiling (attempt 15)

---
+++ scripts/ci_artifacts.mjs
@@ -0,0 +1,35 @@
+import { readdir, rename, mkdir } from 'node:fs'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const ROOT_DIR = join(dirname(fileURLToPath(import.meta.url)), '..')
+const NODE_ARTIFACTS = join(ROOT_DIR, 'bindings', 'node', 'artifacts')
+const ARTIFACTS = join(ROOT_DIR, 'artifacts')
+
+const execute = (command, cwd) =>
+  new Promise((resolve, reject) =>
+    exec(command, { cwd }, (error) =>
+      error ? reject(error?.stack) : resolve()
+    )
+  )
+
+const [artifacts] = await Promise.all([
+  new Promise(resolve => readdir(ARTIFACTS, resolve))
+  new Promise(resolve => mkdir(NODE_ARTIFACTS, resolve))
+])
+
+void await Promise.all(artifacts.map(artifact => {
+  if (artifact.startsWith('native-')) {
+	return execute(`zip ../decancer-${artifact.slice(7)}.zip ./${artifact}/*.node`, ARTIFACTS)
+  } else {
+	return new Promise(resolve => {
+	  const artifactsDir = join(NODE_ARTIFACTS, artifact.replace(/^node\-/, 'bindings-'))
+	  const originDir = join(ARTIFACTS, artifact)
+	  
+	  Promise.all([
+	    new Promise(resolve2 => readdir(originDir, resolve2)),
+	    new Promise(resolve2 => mkdir(artifactsDir, resolve2))
+	  ]).then(([[nodeBinary]]) => mkdir(artifactsDir, () => rename(join(originDir, nodeBinary), join(artifactsDir, nodeBinary), resolve)))
+	})
+  }
+}))
