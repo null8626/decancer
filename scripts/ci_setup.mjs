1d7bdf242679eb2c4ce23b04118fcc2ef6b5674f
null8626 <vierofernando9@gmail.com>
2023-01-23 23:13:09 +0700

    1.5.3

--- scripts/ci_setup.mjs
+++ scripts/ci_setup.mjs
@@ -21,6 +21,8 @@ const coreAffected = files.some(
     filename.startsWith('core/src/') || filename === 'core/bin/confusables.bin'
 )
 
+console.log(`message: "${message}"\ntest: "${/^\d+\.\d+\.\d+$/.test(message)}"`)
+
 appendFileSync(
   process.env.GITHUB_OUTPUT,
   Object.entries({
