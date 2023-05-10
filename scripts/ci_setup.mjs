9875bbe827c56f9dab4bac30c39b4c95af67a65e
null8626 <vierofernando9@gmail.com>
2023-05-10 22:47:15 +0700

    feat: add lambdas

--- scripts/ci_setup.mjs
+++ scripts/ci_setup.mjs
@@ -25,15 +25,9 @@ appendFileSync(
       ? process.env.COMMIT_MESSAGE
       : 'null',
     core_affected: coreAffected,
-    node_affected:
-      coreAffected ||
-      files.some(({ filename }) => filename.startsWith('bindings/node/src')),
-    wasm_affected:
-      coreAffected ||
-      files.some(({ filename }) => filename.startsWith('bindings/wasm/src')),
-    native_affected:
-      coreAffected ||
-      files.some(
+    node_affected: files.some(({ filename }) => filename.startsWith('bindings/node/src')),
+    wasm_affected: coreAffected || files.some(({ filename }) => filename.startsWith('bindings/wasm/src')),
+    native_affected: files.some(
         ({ filename }) =>
           filename.startsWith('bindings/native/src') ||
           filename === 'bindings/native/decancer.h'
