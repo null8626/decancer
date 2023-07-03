33dee4fea267fb0ac212f2366d2f13ca85d1f54b
null8626 <vierofernando9@gmail.com>
2023-07-04 00:47:11 +0700

    refactor: update tets

--- core/src/test.rs
+++ core/src/test.rs
@@ -1,28 +1,23 @@
 use proptest::prelude::*;
 
 proptest! {
-    // Locally, try running a more exhaustive battery with an environment
-    // variable setting like PROPTEST_CASES=1000000.
-    #![proptest_config(ProptestConfig::with_cases(2000))]
+  #![proptest_config(ProptestConfig::with_cases(2000))]
 
-    #[test]
-    fn character_crash(c in any::<char>(), i in any::<u32>()) {
-        let _ = crate::cure_char(c);
-        let _ = crate::cure_char(i);
-    }
+  #[test]
+  fn character_crash(c in any::<char>()) {
+    let _ = crate::cure_char(c);
+  }
 
-    #[test]
-    #[cfg(feature = "std")]
-    fn string_crash(s in "\\PC*") {
-        let _ = crate::cure(&s);
-    }
+  #[test]
+  #[cfg(feature = "std")]
+  fn string_crash(s in "\\PC*") {
+    let _ = crate::cure(&s);
+  }
 }
 
 #[test]
 fn regression_crash() {
-  let _ = crate::cure_char('Æ');
-  let _ = crate::cure("Æ");
-
-  let _ = crate::cure_char('ˑ');
-  let _ = crate::cure("ˑ");
+  let _ = crate::cure_char('\u{c0}');
+  let _ = crate::cure_char('\u{16e5f}');
+  let _ = crate::cure_char(char::MAX);
 }
