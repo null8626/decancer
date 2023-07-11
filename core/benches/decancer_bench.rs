e8b3492e9a8ada05058663221751de999e4049b6
Yossipossi <yossipossi770@gmail.com>
2023-07-11 14:29:39 -0700

    Add comments describing the purpose of the benchmark functions

--- core/benches/decancer_bench.rs
+++ core/benches/decancer_bench.rs
@@ -2,12 +2,14 @@ use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use decancer::{cure, cure_char};
 use rand::{thread_rng, Rng};
 
+/// Tests the speed of curing a short constant `String`.
 fn cure_short(c: &mut Criterion) {
     let input = String::from("vＥⓡ𝔂 𝔽𝕌Ňℕｙ ţ乇𝕏𝓣");
   
     c.bench_function("cure short input", |b| b.iter(|| cure(&input)));
   }
 
+/// Tests the speed of curing a random input `String` with 500 characters.
 fn cure_random(c: &mut Criterion) {
   c.bench_function("cure random input", |b| {
     b.iter_batched(
@@ -23,6 +25,7 @@ fn cure_random(c: &mut Criterion) {
   });
 }
 
+/// Tests the speed of curing indvidual random characters using `cure_char`.
 fn cure_char_random(c: &mut Criterion) {
   c.bench_function("cure_char random input", |b| {
     b.iter_batched(
