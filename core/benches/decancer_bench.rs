3e4e2dc7634ed69eaa5b0d13a397b93fb5278af7
Yossipossi <yossipossi770@gmail.com>
2023-07-11 14:27:51 -0700

    Remove unnecessary tests and increase size of cure test input

--- core/benches/decancer_bench.rs
+++ core/benches/decancer_bench.rs
@@ -2,13 +2,19 @@ use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use decancer::{cure, cure_char};
 use rand::{thread_rng, Rng};
 
+fn cure_short(c: &mut Criterion) {
+    let input = String::from("vＥⓡ𝔂 𝔽𝕌Ňℕｙ ţ乇𝕏𝓣");
+  
+    c.bench_function("cure short input", |b| b.iter(|| cure(&input)));
+  }
+
 fn cure_random(c: &mut Criterion) {
-  c.bench_function("cure random batch", |b| {
+  c.bench_function("cure random input", |b| {
     b.iter_batched(
       || {
         thread_rng()
           .sample_iter::<char, _>(rand::distributions::Standard)
-          .take(100)
+          .take(500)
           .collect::<String>()
       },
       |key| cure(&key),
@@ -18,7 +24,7 @@ fn cure_random(c: &mut Criterion) {
 }
 
 fn cure_char_random(c: &mut Criterion) {
-  c.bench_function("cure_char random batch", |b| {
+  c.bench_function("cure_char random input", |b| {
     b.iter_batched(
       || {
         thread_rng()
@@ -32,23 +38,10 @@ fn cure_char_random(c: &mut Criterion) {
   });
 }
 
-fn cure_speed(c: &mut Criterion) {
-  let input = String::from("vＥⓡ𝔂 𝔽𝕌Ňℕｙ ţ乇𝕏𝓣");
-
-  c.bench_function("cure speed", |b| b.iter(|| cure(&input)));
-}
-
-fn cure_char_speed(c: &mut Criterion) {
-  let input = '✅';
-
-  c.bench_function("cure_char speed", |b| b.iter(|| cure_char(input)));
-}
-
 criterion_group!(
   benches,
-  cure_speed,
+  cure_short,
   cure_random,
-  cure_char_speed,
   cure_char_random,
 );
 criterion_main!(benches);
