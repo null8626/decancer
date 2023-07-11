b170138a65a1f19c88311e5bb5890683c8d221e5
Yossipossi <yossipossi770@gmail.com>
2023-07-11 13:32:46 -0700

    Rename and reorder bench tests

--- core/benches/decancer_bench.rs
+++ core/benches/decancer_bench.rs
@@ -3,7 +3,7 @@ use decancer::{cure, cure_char};
 use rand::{thread_rng, Rng};
 
 fn cure_random(c: &mut Criterion) {
-  c.bench_function("cure_random", |b| {
+  c.bench_function("cure random batch", |b| {
     b.iter_batched(
       || {
         thread_rng()
@@ -18,7 +18,7 @@ fn cure_random(c: &mut Criterion) {
 }
 
 fn cure_char_random(c: &mut Criterion) {
-  c.bench_function("cure_char_random", |b| {
+  c.bench_function("cure_char random batch", |b| {
     b.iter_batched(
       || {
         thread_rng()
@@ -35,20 +35,20 @@ fn cure_char_random(c: &mut Criterion) {
 fn cure_speed(c: &mut Criterion) {
   let input = String::from("vＥⓡ𝔂 𝔽𝕌Ňℕｙ ţ乇𝕏𝓣");
 
-  c.bench_function("cure_speed", |b| b.iter(|| cure(&input)));
+  c.bench_function("cure speed", |b| b.iter(|| cure(&input)));
 }
 
 fn cure_char_speed(c: &mut Criterion) {
   let input = '✅';
 
-  c.bench_function("cure_char_speed", |b| b.iter(|| cure_char(input)));
+  c.bench_function("cure_char speed", |b| b.iter(|| cure_char(input)));
 }
 
 criterion_group!(
   benches,
-  cure_random,
-  cure_char_random,
   cure_speed,
-  cure_char_speed
+  cure_random,
+  cure_char_speed,
+  cure_char_random,
 );
 criterion_main!(benches);
