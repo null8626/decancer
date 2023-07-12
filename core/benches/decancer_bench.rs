fcb1c7baa6feb4f20d3230efe1e5b5ef8aabeeda
Yossipossi <yossipossi770@gmail.com>
2023-07-11 20:14:10 -0700

    Change method of string/char generation in benches
    (thanks null!)

--- core/benches/decancer_bench.rs
+++ core/benches/decancer_bench.rs
@@ -1,24 +1,19 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use decancer::{cure, cure_char};
-use rand::{thread_rng, Rng};
+use rand::random;
 
 /// Tests the speed of curing a short constant `String`.
 fn cure_short(c: &mut Criterion) {
-    let input = String::from("vＥⓡ𝔂 𝔽𝕌Ňℕｙ ţ乇𝕏𝓣");
-  
-    c.bench_function("cure short input", |b| b.iter(|| cure(&input)));
-  }
+  let input = String::from("vＥⓡ𝔂 𝔽𝕌Ňℕｙ ţ乇𝕏𝓣");
+
+  c.bench_function("cure short input", |b| b.iter(|| cure(&input)));
+}
 
 /// Tests the speed of curing a random input `String` with 500 characters.
 fn cure_random(c: &mut Criterion) {
   c.bench_function("cure random input", |b| {
     b.iter_batched(
-      || {
-        thread_rng()
-          .sample_iter::<char, _>(rand::distributions::Standard)
-          .take(500)
-          .collect::<String>()
-      },
+      || String::from_iter([random::<char>(); 500].iter()),
       |key| cure(&key),
       BatchSize::SmallInput,
     )
@@ -28,23 +23,9 @@ fn cure_random(c: &mut Criterion) {
 /// Tests the speed of curing indvidual random characters using `cure_char`.
 fn cure_char_random(c: &mut Criterion) {
   c.bench_function("cure_char random input", |b| {
-    b.iter_batched(
-      || {
-        thread_rng()
-          .sample_iter::<char, _>(rand::distributions::Standard)
-          .next()
-          .unwrap()
-      },
-      cure_char,
-      BatchSize::SmallInput,
-    )
+    b.iter_batched(|| random::<char>(), cure_char, BatchSize::SmallInput)
   });
 }
 
-criterion_group!(
-  benches,
-  cure_short,
-  cure_random,
-  cure_char_random,
-);
+criterion_group!(benches, cure_short, cure_random, cure_char_random,);
 criterion_main!(benches);
