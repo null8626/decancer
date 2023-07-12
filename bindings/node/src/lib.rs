624b4dc8c2fc0e72557c8aad0c1e3035218cfa7d
null8626 <vierofernando9@gmail.com>
2023-07-12 22:13:38 +0700

    feat: new codepoints and fixes

--- bindings/node/src/lib.rs
+++ bindings/node/src/lib.rs
@@ -3,7 +3,7 @@
 #[macro_use]
 extern crate napi_derive;
 
-use napi::{Env, JsBuffer, JsString, Result};
+use napi::{Env, JsString, Result};
 
 #[napi]
 pub struct CuredString(decancer::CuredString);
@@ -28,13 +28,6 @@ impl CuredString {
   #[napi]
   pub fn equals(&self, other: String) -> bool {
     self.0 == other
-  }
-
-  #[napi]
-  pub fn to_buffer(&self, env: Env) -> Result<JsBuffer> {
-    env
-      .create_buffer_with_data(self.0.clone().into_str().into_bytes())
-      .map(|b| b.into_raw())
   }
 
   #[napi]
