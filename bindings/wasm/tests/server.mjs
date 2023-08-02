a25451118dd21bfd2be6ed069fdfe9d5fde6965f
null8626 <vierofernando9@gmail.com>
2023-08-02 19:53:32 +0700

    feat: add wasm tests

---
+++ bindings/wasm/tests/server.mjs
@@ -0,0 +1,43 @@
+import { parentPort } from 'node:worker_threads'
+import fastifyStatic from '@fastify/static'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createReadStream } from 'node:fs'
+import fastify from 'fastify'
+
+const CURRENT_DIR = dirname(fileURLToPath(import.meta.url))
+
+console.log('- [server] running...')
+
+const app = fastify()
+
+app.register(fastifyStatic, {
+  root: join(CURRENT_DIR, '..', 'bin')
+})
+
+app.get('/', (req, res) => res.type('text/html').send(createReadStream(join(CURRENT_DIR, 'index.html'))))
+
+app.listen({
+  port: 8080
+}, err => {
+  if (err) {
+    parentPort.postMessage({
+      code: 'error',
+      stack: err.stack
+    })
+  } else {
+    console.log('- [server] ready.')
+    
+    parentPort.postMessage({
+      code: 'ready'
+    })
+  
+    parentPort.on('message', () => {
+      console.log('- [server] closing...')
+      
+      app.close().finally(() => parentPort.postMessage({
+        code: 'close'
+      }))
+    })
+  }
+})
