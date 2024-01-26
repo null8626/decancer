00c95d0d4940d5bdcecefdb2c37361b99d5ab2c1
null8626 <null8626@users.noreply.github.com>
2024-01-26 13:09:11 +0000

    chore: prettier and update wasm

--- bindings/node/test.cjs
+++ bindings/node/test.cjs
@@ -3,21 +3,23 @@ const { describe, it } = require('node:test')
 
 class TestContext {
   #inner
-  
+
   constructor(result) {
     this.#inner = result
   }
-  
+
   test(functionName, expected, ...args) {
-    it(functionName, () => strictEqual(this.#inner[functionName](...args), expected))
-    
+    it(functionName, () =>
+      strictEqual(this.#inner[functionName](...args), expected)
+    )
+
     return this
   }
 }
 
 describe('decancer', () => {
   const decancer = require('./src/lib.js')
-  
+
   new TestContext(decancer('vＥⓡ𝔂 𝔽𝕌Ňℕｙ ţ乇𝕏𝓣'))
     .test('equals', true, 'very funny text')
     .test('startsWith', true, 'very')
