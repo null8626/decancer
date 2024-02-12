c09dd6f8b016c849d13aeca255358a5b2577342e
null8626 <vierofernando9@gmail.com>
2024-02-12 15:10:44 +0700

    ci: attempt to support more targets (attempt 4)

---
+++ scripts/ci_native_artifacts.mjs
@@ -0,0 +1,34 @@
+import { readdir, rename, mkdir } from 'node:fs/promises'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { promisify } from 'node:util'
+
+const ROOT_DIR = join(dirname(fileURLToPath(import.meta.url)), '..')
+
+try {
+  const [artifacts] = await Promise.all([readdir(join(ROOT_DIR, 'bindings', 'native', 'target', 'release')), mkdir(join(ROOT_DIR, 'artifacts'))])
+  
+  const promises = []
+  
+  for (const artifact of artifacts) {
+    try {
+      const [ext] = artifact.match(/\.[^\.]+$/)[0].slice(1)
+      
+      if (ext === 'lib' || ext === 'dll' || ext === 'so' || ext === 'dylib') {
+        promises.push(rename(join(ROOT_DIR, 'bindings', 'native', 'target', 'release', artifact), join(ROOT_DIR, 'artifacts')))
+      }
+    } catch {
+      continue
+    }
+  }
+  
+  if (promises.length === 0) {
+    // throw me away
+    throw null
+  }
+  
+  void await Promise.all(promises)
+} catch {
+  console.error('- error: target directory is empty')
+  process.exit(1)
+}
