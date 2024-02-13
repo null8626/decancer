b397e25b8b8b7b60db37a8d4a5aacaf24674adc4
null8626 <vierofernando9@gmail.com>
2024-02-14 00:49:10 +0700

    feat: add java again (attempt 16)

--- scripts/ci_native_artifacts.mjs
+++ scripts/ci_native_artifacts.mjs
@@ -3,7 +3,6 @@ import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const TARGET = process.argv[2]
-const IS_MOVE = process.argv.some(argv => argv === '--move')
 const IS_JAVA = process.argv.some(argv => argv === '--java')
 
 const ROOT_DIR = join(dirname(fileURLToPath(import.meta.url)), '..')
@@ -13,42 +12,36 @@ const TARGET_DIR = join(
   IS_JAVA ? 'java' : 'native',
   TARGET
 )
-const ARTIFACTS_DIR = join(ROOT_DIR, 'artifacts')
 
-if (IS_MOVE) {
-  const artifacts = await readdir(join(TARGET_DIR, 'release'))
-  const promises = []
+const artifacts = await readdir(join(TARGET_DIR, 'release'))
+const promises = []
 
-  for (const artifact of artifacts) {
-    try {
-      const ext = artifact.match(/\.\w+$/)[0].slice(1)
+for (const artifact of artifacts) {
+  try {
+    const ext = artifact.match(/\.\w+$/)[0].slice(1)
 
-      if (ext === 'lib' || ext === 'dll' || ext === 'so' || ext === 'dylib') {
-        promises.push(
-          rename(
-            join(TARGET_DIR, 'release', artifact),
-            join(ARTIFACTS_DIR, artifact.replace('.dll.lib', '.lib'))
-          )
-        )
+    if (ext === 'lib' || ext === 'dll' || ext === 'so' || ext === 'dylib') {
+      let newPath = join(ROOT_DIR, 'artifacts', artifact.replace('.dll.lib', '.lib'))
+      
+      if (IS_JAVA) {
+        newPath = newPath.replace('decancer', `decancer-${TARGET}`)
       }
-    } catch {
-      continue
+      
+      promises.push(
+        rename(
+          join(TARGET_DIR, 'release', artifact),
+          newPath
+        )
+      )
     }
-  }
-
-  if (promises.length === 0) {
-    console.error('error: target directory is empty')
-    process.exit(1)
+  } catch {
+    continue
   }
 }
 
-if (IS_JAVA) {
-  const binaries = await readdir(ARTIFACTS_DIR)
-
-  for (const binary of binaries) {
-    await rename(
-      join(ARTIFACTS_DIR, binary),
-      join(ARTIFACTS_DIR, binary.replace('decancer', `decancer-${TARGET}`))
-    )
-  }
+if (promises.length === 0) {
+  console.error('error: target directory is empty')
+  process.exit(1)
 }
+
+void await Promise.all(promises)
