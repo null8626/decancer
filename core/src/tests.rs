c55bb10b9c538114f959b8db996bb8fd46c5b409
null8626 <null8626@users.noreply.github.com>
2024-02-27 04:00:03 +0000

    Deploying to gh-pages from @ null8626/decancer@dfbeceb5373984692a0858c3f39407ebbbc252ae 🚀

--- core/src/tests.rs
+++ core/src/tests.rs
@@ -53,17 +53,19 @@ fn similar_contains() {
     };
   }
 
-  //assert!(is_contains!("hello", "hello"));
-  //assert!(is_contains!("hello?", "hello"));
-  //assert!(is_contains!("hhheeeeelllloo!!", "hello"));
-  //assert!(is_contains!("hh-he  e ee!e!ll/l/lo//o-?", "hello"));
-  //
-  //assert!(is_contains!("-!?hel$2-hello?", "hello"));
-  //assert!(is_contains!("-!?hel$2-hhheeeeelllloo!!", "hello"));
-  //assert!(is_contains!("-!?hel$2-hh-he  e ee!e!ll/l/lo//o-?", "hello"));
+  assert!(is_contains!("hello", "hello"));
+  assert!(is_contains!("hello?", "hello"));
+  assert!(is_contains!("hhheeeeelllloo!!", "hello"));
+  assert!(is_contains!("hh-he  e ee!e!ll/l/lo//o-?", "hello"));
 
+  assert!(is_contains!("-!?hel$2-hello?", "hello"));
+  assert!(is_contains!("-!?hel$2-hhheeeeelllloo!!", "hello"));
+  assert!(is_contains!("-!?hel$2-hh-he  e ee!e!ll/l/lo//o-?", "hello"));
+
+  assert!(!is_contains!("eel", "ell"));
+  assert!(!is_contains!("ell", "eel"));
   assert!(!is_contains!("-!?hel", "hell"));
-  //assert!(!is_contains!("ello?", "hello"));
+  assert!(!is_contains!("ello?", "hello"));
 }
 
 #[test]
