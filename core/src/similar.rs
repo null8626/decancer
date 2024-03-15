48a5cae82b42db80b80a198fbb32c9c3a84f4f19
null8626 <null8626@users.noreply.github.com>
2024-03-15 11:23:43 +0000

    chore: update files

--- core/src/similar.rs
+++ core/src/similar.rs
@@ -87,7 +87,9 @@ impl<'a, 'b> Matcher<'a, 'b> {
 
   fn matches(&mut self, self_char: char, other_char: char) -> Option<usize> {
     if cfg!(feature = "leetspeak") {
-      self.matches_leetspeak(other_char).or_else(|| self.matches_character(self_char, other_char))
+      self
+        .matches_leetspeak(other_char)
+        .or_else(|| self.matches_character(self_char, other_char))
     } else {
       self.matches_character(self_char, other_char)
     }
@@ -120,7 +122,7 @@ impl<'a, 'b> Iterator for Matcher<'a, 'b> {
 
   fn next(&mut self) -> Option<Self::Item> {
     self.other_iterator.restart();
-    
+
     let mut current_other = self.other_iterator.next()?;
 
     let (skipped, matched_skip) = self.skip_until(current_other.0)?;
