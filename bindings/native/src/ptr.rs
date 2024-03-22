53a46eb1c5903ab0e2ef88d82c3f7b42c71b9e97
null8626 <vierofernando9@gmail.com>
2024-03-23 06:43:56 +0700

    feat: add multiple methods

---
+++ bindings/native/src/ptr.rs
@@ -0,0 +1,64 @@
+use std::mem::size_of;
+
+pub(crate) struct NullTerminatedPointer<T> {
+  ptr: *mut T,
+  pub(crate) size: usize,
+}
+
+impl<T> NullTerminatedPointer<T> {
+  pub(crate) const fn new(ptr: *mut T) -> Self {
+    Self { ptr, size: 0 }
+  }
+}
+
+impl<T> Iterator for NullTerminatedPointer<T>
+where
+  T: PartialEq<T> + Default + Copy,
+{
+  type Item = T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    let value = unsafe { *self.ptr };
+
+    self.ptr = unsafe { self.ptr.offset(1) };
+
+    if value == Default::default() {
+      None
+    } else {
+      self.size += size_of::<T>();
+
+      Some(value)
+    }
+  }
+}
+
+pub(crate) struct SizedPointer<T> {
+  ptr: *mut T,
+  size: usize,
+}
+
+impl<T> SizedPointer<T> {
+  pub(crate) const fn new(ptr: *mut T, size: usize) -> Self {
+    Self { ptr, size }
+  }
+}
+
+impl<T> Iterator for SizedPointer<T>
+where
+  T: Copy,
+{
+  type Item = T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.size == 0 {
+      return None;
+    }
+
+    let value = unsafe { *self.ptr };
+
+    self.ptr = unsafe { self.ptr.offset(1) };
+    self.size -= size_of::<T>();
+
+    Some(value)
+  }
+}
