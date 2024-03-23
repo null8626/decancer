43ade239e937af3aa8943b99839359cc1f76140d
null8626 <null8626@users.noreply.github.com>
2024-03-23 23:32:14 +0000

    chore: update files

--- bindings/java/src/test/java/com/github/null8626/decancer/DecancerTest.java
+++ bindings/java/src/test/java/com/github/null8626/decancer/DecancerTest.java
@@ -10,14 +10,14 @@ public class DecancerTest {
   public void cure() {
     this.cured = new CuredString("vＥⓡ𝔂 𝔽𝕌Ňℕｙ ţ乇𝕏𝓣");
   }
-  
+
   @Test
   public void censor() throws Exception {
     CuredString string = new CuredString("wow heellllo wow hello wow!");
-  
+
     try {
       string.censor("hello", '*');
-      
+
       Assertions.assertEquals(string.toString(), "wow ******** wow ***** wow!");
       string.destroy();
     } catch (Exception err) {
@@ -25,15 +25,15 @@ public class DecancerTest {
       throw err;
     }
   }
-  
+
   @Test
   public void censorMultiple() throws Exception {
     CuredString string = new CuredString("helloh yeah");
-  
+
     try {
       String[] keywords = { "hello", "oh yeah" };
       string.censorMultiple(keywords, '*');
-      
+
       Assertions.assertEquals(string.toString(), "***********");
       string.destroy();
     } catch (Exception err) {
@@ -41,15 +41,15 @@ public class DecancerTest {
       throw err;
     }
   }
-  
+
   @Test
   public void replace() throws Exception {
     CuredString string = new CuredString("wow hello wow heellllo!");
     Exception exception = null;
-  
+
     try {
       string.replace("hello", "world");
-      
+
       Assertions.assertEquals(string.toString(), "wow world wow world!");
       string.destroy();
     } catch (Exception err) {
@@ -57,15 +57,15 @@ public class DecancerTest {
       throw err;
     }
   }
-  
+
   @Test
   public void replaceMultiple() throws Exception {
     CuredString string = new CuredString("helloh yeah");
-  
+
     try {
       String[] keywords = { "hello", "oh yeah" };
       string.replaceMultiple(keywords, "world");
-      
+
       Assertions.assertEquals(string.toString(), "world");
       string.destroy();
     } catch (Exception err) {
