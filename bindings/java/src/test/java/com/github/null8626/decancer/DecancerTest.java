48c26032d97de500739575f6540adc7a0fbf8484
null8626 <vierofernando9@gmail.com>
2024-03-24 06:28:34 +0700

    fix: fix java tests

--- bindings/java/src/test/java/com/github/null8626/decancer/DecancerTest.java
+++ bindings/java/src/test/java/com/github/null8626/decancer/DecancerTest.java
@@ -10,86 +10,67 @@ public class DecancerTest {
   public void cure() {
     this.cured = new CuredString("vＥⓡ𝔂 𝔽𝕌Ňℕｙ ţ乇𝕏𝓣");
   }
-
+  
   @Test
-  public void censor() {
+  public void censor() throws Exception {
     CuredString string = new CuredString("wow heellllo wow hello wow!");
-    Exception exception = null;
-
+  
     try {
       string.censor("hello", '*');
-
+      
       Assertions.assertEquals(string.toString(), "wow ******** wow ***** wow!");
-    } catch (Exception err) {
-      exception = err;
-    } finally {
       string.destroy();
-
-      if (exception != null) {
-        throw exception;
-      }
+    } catch (Exception err) {
+      string.destroy();
+      throw err;
     }
   }
-
+  
   @Test
-  public void censorMultiple() {
+  public void censorMultiple() throws Exception {
     CuredString string = new CuredString("helloh yeah");
-    Exception exception = null;
-
+  
     try {
       String[] keywords = { "hello", "oh yeah" };
       string.censorMultiple(keywords, '*');
-
+      
       Assertions.assertEquals(string.toString(), "***********");
-    } catch (Exception err) {
-      exception = err;
-    } finally {
       string.destroy();
-
-      if (exception != null) {
-        throw exception;
-      }
+    } catch (Exception err) {
+      string.destroy();
+      throw err;
     }
   }
-
+  
   @Test
-  public void replace() {
+  public void replace() throws Exception {
     CuredString string = new CuredString("wow hello wow heellllo!");
     Exception exception = null;
-
+  
     try {
       string.replace("hello", "world");
-
+      
       Assertions.assertEquals(string.toString(), "wow world wow world!");
-    } catch (Exception err) {
-      exception = err;
-    } finally {
       string.destroy();
-
-      if (exception != null) {
-        throw exception;
-      }
+    } catch (Exception err) {
+      string.destroy();
+      throw err;
     }
   }
-
+  
   @Test
-  public void replaceMultiple() {
+  public void replaceMultiple() throws Exception {
     CuredString string = new CuredString("helloh yeah");
-    Exception exception = null;
-
+  
     try {
       String[] keywords = { "hello", "oh yeah" };
       string.replaceMultiple(keywords, "world");
-
+      
       Assertions.assertEquals(string.toString(), "world");
-    } catch (Exception err) {
-      exception = err;
-    } finally {
       string.destroy();
-
-      if (exception != null) {
-        throw exception;
-      }
+    } catch (Exception err) {
+      string.destroy();
+      throw err;
     }
   }
 
