22f3f32ab5c2f7ec0145fbd1921cde5cce9ff3ef
null8626 <vierofernando9@gmail.com>
2024-03-24 08:02:30 +0700

    refactor: use bash timeout command [skip ci]

--- bindings/wasm/tests/server.mjs
+++ bindings/wasm/tests/server.mjs
@@ -38,14 +38,10 @@ app.listen(
 
       parentPort.on('message', () => {
         console.log('- [server] closing...')
-        const notifyClose = () =>
-          parentPort.postMessage({
-            code: 'close'
-          })
 
-        // this sometimes takes like a billion years to finish for some reason
-        setTimeout(notifyClose, 30000)
-        app.close().finally(notifyClose)
+        app.close().finally(() => parentPort.postMessage({
+          code: 'close'
+        }))
       })
     }
   }
