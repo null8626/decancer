144f0931770934d5629eeebbca792adad3c7dd3b
null8626 <vierofernando9@gmail.com>
2024-04-09 17:20:54 +0700

    fix: setup-rust no longer exists

--- scripts/ci_pages_gitignore.mjs
+++ scripts/ci_pages_gitignore.mjs
@@ -11,33 +11,52 @@ const ROOT_DIR = join(dirname(fileURLToPath(import.meta.url)), '..')
 const PATH_TO_INDEX_HTML = join(ROOT_DIR, 'index.html')
 const PATH_TO_WASM_BIN = join(ROOT_DIR, 'bindings/wasm/bin/')
 
-const notGitignored = (await execute('git ls-files --cached --others --exclude-standard')).stdout.toString().trim().split(/\r?\n/g).map(path => join(ROOT_DIR, path))
-const gitignore = (await readFile(join(ROOT_DIR, '.gitignore'))).toString().trim()
+const notGitignored = (
+  await execute('git ls-files --cached --others --exclude-standard')
+).stdout
+  .toString()
+  .trim()
+  .split(/\r?\n/g)
+  .map(path => join(ROOT_DIR, path))
+const gitignore = (await readFile(join(ROOT_DIR, '.gitignore')))
+  .toString()
+  .trim()
 const rootFiles = await readdir(ROOT_DIR)
 
 function isNotExcluded(fullPath) {
   const file = basename(fullPath)
-  
-  return fullPath === PATH_TO_INDEX_HTML || (fullPath.startsWith(PATH_TO_WASM_BIN) && (file === 'decancer.wasm' || file === 'decancer.min.js'))
+
+  return (
+    fullPath === PATH_TO_INDEX_HTML ||
+    (fullPath.startsWith(PATH_TO_WASM_BIN) &&
+      (file === 'decancer.wasm' || file === 'decancer.min.js'))
+  )
 }
 
 async function resolveDirectory(directoryName) {
   const files = await readdir(directoryName)
   const output = []
-  
-  void await Promise.all(files.map(path => join(directoryName, path)).map(async path => {
-    const fstat = await stat(path)
-    
-    if (fstat.isDirectory()) {
-      output.push(...await resolveDirectory(path))
-    } else if (notGitignored.includes(path) && !isNotExcluded(path)) {
-      output.push(path)
-    }
-  }))
-  
+
+  void (await Promise.all(
+    files
+      .map(path => join(directoryName, path))
+      .map(async path => {
+        const fstat = await stat(path)
+
+        if (fstat.isDirectory()) {
+          output.push(...(await resolveDirectory(path)))
+        } else if (notGitignored.includes(path) && !isNotExcluded(path)) {
+          output.push(path)
+        }
+      })
+  ))
+
   return output
 }
 
 const nonPageFiles = await resolveDirectory(ROOT_DIR)
 
-await writeFile(join(ROOT_DIR, '.gitignore'), `${gitignore}\n${nonPageFiles.join('\n')}`)
+await writeFile(
+  join(ROOT_DIR, '.gitignore'),
+  `${gitignore}\n${nonPageFiles.join('\n')}`
+)
