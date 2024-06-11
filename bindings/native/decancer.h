7918fed32d4c9ae1e430754cd9a0de74e67a1b06
null8626 <vierofernando9@gmail.com>
2024-06-11 13:23:47 +0700

    fix: ci fix attempt 3

--- bindings/native/decancer.h
+++ bindings/native/decancer.h
@@ -908,6 +908,9 @@ extern "C" {
    *     return 1;
    *   }
    *
+   *   const uint8_t* f = (const uint8_t*)funny;
+   *   printf("%d %d %d %d %d %d %d %d %d %d\n", f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9]);
+   *
    *   matcher = decancer_find_utf16(cured, funny, sizeof(funny) / sizeof(uint16_t));
    *   decancer_assert(matcher != NULL, "decancer_find_utf16", CURED_END);
    *
