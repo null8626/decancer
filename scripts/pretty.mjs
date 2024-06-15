79b36d7a5643025d6e525f4f09f79bb5eab57827
null8626 <vierofernando9@gmail.com>
2024-06-15 17:55:27 +0700

    refactor: ignore unaffected portions of the codebase

--- scripts/pretty.mjs
+++ scripts/pretty.mjs
@@ -18,7 +18,15 @@ const RANGE_MASK = 0x8000000
 const ROOT_DIR = join(dirname(fileURLToPath(import.meta.url)), '..')
 const CORE_DIR = join(ROOT_DIR, 'core')
 const BINDINGS_DIR = join(ROOT_DIR, 'bindings')
+const SETUP_OUTPUTS = process.env.DECANCER_SETUP_OUTPUTS ? JSON.parse(process.env.DECANCER_SETUP_OUTPUTS) : null
 const OPTIONS = options(process.argv.slice(2))
+
+function isAffected(value) {
+  return SETUP_OUTPUTS !== null
+    ? SETUP_OUTPUTS.release !== 'null' ||
+        SETUP_OUTPUTS[`${value}_affected`] === 'true'
+    : true
+}
 
 if (!existsSync(join(ROOT_DIR, '.cache.bin'))) {
   execSync(`node ${join(ROOT_DIR, 'scripts', 'update_unicode.mjs')}`, {
@@ -33,89 +41,98 @@ const { alreadyHandledCount } = deserialize(
 const execute = promisify(exec)
 
 async function updateReadme() {
-  console.log('- [readme] reading codepoints.bin...')
+  if (isAffected('core')) {
+    console.log('- [readme] reading codepoints.bin...')
 
-  const bin = await readFile(join(CORE_DIR, 'bin', 'codepoints.bin'))
+    const bin = await readFile(join(CORE_DIR, 'bin', 'codepoints.bin'))
 
-  console.log('- [readme] parsing codepoints.bin...')
+    console.log('- [readme] parsing codepoints.bin...')
 
-  let codepointsCount = NONE_CODEPOINTS_COUNT + alreadyHandledCount
+    let codepointsCount = NONE_CODEPOINTS_COUNT + alreadyHandledCount
 
-  const codepointsEnd = bin.readUint16LE()
-  const caseSensitiveCodepointsEnd = bin.readUint16LE(2)
-  const caseSensitiveCodepoints = []
-  let offset = codepointsEnd
+    const codepointsEnd = bin.readUint16LE()
+    const caseSensitiveCodepointsEnd = bin.readUint16LE(2)
+    const caseSensitiveCodepoints = []
+    let offset = codepointsEnd
 
-  for (; offset < caseSensitiveCodepointsEnd; offset += 6) {
-    const integer = bin.readUint32LE(offset)
+    for (; offset < caseSensitiveCodepointsEnd; offset += 6) {
+      const integer = bin.readUint32LE(offset)
 
-    const codepoint = integer & CODEPOINT_MASK
-    let toAdd = 1
+      const codepoint = integer & CODEPOINT_MASK
+      let toAdd = 1
 
-    caseSensitiveCodepoints.push(codepoint)
+      caseSensitiveCodepoints.push(codepoint)
 
-    if ((integer & RANGE_MASK) !== 0) {
-      const rangeUntil = bin.readUint8(offset + 4) & 0x7f
+      if ((integer & RANGE_MASK) !== 0) {
+        const rangeUntil = bin.readUint8(offset + 4) & 0x7f
 
-      caseSensitiveCodepoints.push(
-        ...Array.from({ length: rangeUntil }, (_, i) => codepoint + 1 + i)
-      )
-      toAdd += rangeUntil
+        caseSensitiveCodepoints.push(
+          ...Array.from({ length: rangeUntil }, (_, i) => codepoint + 1 + i)
+        )
+        toAdd += rangeUntil
+      }
+
+      codepointsCount += toAdd
     }
 
-    codepointsCount += toAdd
+    for (offset = 6; offset < codepointsEnd; offset += 6) {
+      const integer = bin.readUint32LE(offset)
+
+      const codepoint = integer & CODEPOINT_MASK
+      let toAdd = 1
+
+      if ((integer & RANGE_MASK) !== 0) {
+        toAdd += bin.readUint8(offset + 4) & 0x7f
+      }
+
+      const uppercasedCodepoint = String.fromCodePoint(codepoint)
+        .toUpperCase()
+        .codePointAt()
+
+      if (
+        uppercasedCodepoint !== codepoint &&
+        !caseSensitiveCodepoints.includes(uppercasedCodepoint)
+      ) {
+        toAdd *= 2
+      }
+
+      codepointsCount += toAdd
+    }
+
+    console.log('- [readme] reading README.md...')
+
+    const readme = await readFile(join(CORE_DIR, 'README.md'))
+
+    await writeFile(
+      join(CORE_DIR, 'README.md'),
+      readme
+        .toString()
+        .trim()
+        .replace(
+          /\*\*[\d,\.]+ \(\d+[\.\,]\d{2}%\) different unicode codepoints\*\*/,
+          `**${codepointsCount.toLocaleString('en-US')} (${(
+            (codepointsCount / 0x10ffff) *
+            100
+          ).toFixed(2)}%) different unicode codepoints**`
+        )
+    )
+
+    console.log('- [readme] updated')
   }
-
-  for (offset = 6; offset < codepointsEnd; offset += 6) {
-    const integer = bin.readUint32LE(offset)
-
-    const codepoint = integer & CODEPOINT_MASK
-    let toAdd = 1
-
-    if ((integer & RANGE_MASK) !== 0) {
-      toAdd += bin.readUint8(offset + 4) & 0x7f
-    }
-
-    const uppercasedCodepoint = String.fromCodePoint(codepoint)
-      .toUpperCase()
-      .codePointAt()
-
-    if (
-      uppercasedCodepoint !== codepoint &&
-      !caseSensitiveCodepoints.includes(uppercasedCodepoint)
-    ) {
-      toAdd *= 2
-    }
-
-    codepointsCount += toAdd
-  }
-
-  console.log('- [readme] reading README.md...')
-
-  const readme = await readFile(join(CORE_DIR, 'README.md'))
-
-  await writeFile(
-    join(CORE_DIR, 'README.md'),
-    readme
-      .toString()
-      .trim()
-      .replace(
-        /\*\*[\d,\.]+ \(\d+[\.\,]\d{2}%\) different unicode codepoints\*\*/,
-        `**${codepointsCount.toLocaleString('en-US')} (${(
-          (codepointsCount / 0x10ffff) *
-          100
-        ).toFixed(2)}%) different unicode codepoints**`
-      )
-  )
-
-  console.log('- [readme] updated')
 }
 
 async function prettier() {
-  await execute('npm i -g prettier')
-  await execute('npm i prettier-plugin-java --save-dev')
+  const extensions = ['js', 'ts', 'mjs', 'cjs', 'json']
 
-  await execute('npx prettier **/*.{js,ts,mjs,cjs,json,java} --write', {
+  await execute('npm i -g prettier')
+
+  if (isAffected('java')) {
+    await execute('npm i prettier-plugin-java --save-dev')
+
+    extensions.push('java')
+  }
+
+  await execute(`npx prettier **/*.{${extensions.join(',')}} --write`, {
     cwd: ROOT_DIR
   })
 
@@ -124,32 +141,36 @@ async function prettier() {
   console.log('- [prettier] completed prettifying files')
 }
 
-async function cargo(cwd) {
-  console.log(`- [cargo -> ${cwd}] running clippy and rustfmt...`)
+async function cargo(cwd, ty) {
+  if (isAffected(ty)) {
+    console.log(`- [cargo -> ${cwd}] running clippy and rustfmt...`)
 
-  await execute('cargo fmt', { cwd })
+    await execute('cargo fmt', { cwd })
 
-  console.log(`- [cargo -> ${cwd}] completed`)
+    console.log(`- [cargo -> ${cwd}] completed`)
+  }
 }
 
 async function clangFormat() {
-  const clangFormatExecutable = OPTIONS['clang-format'] ?? 'clang-format'
+  if (isAffected('native')) {
+    const clangFormatExecutable = OPTIONS['clang-format'] ?? 'clang-format'
 
-  console.log(`- [${clangFormatExecutable}] running...`)
+    console.log(`- [${clangFormatExecutable}] running...`)
 
-  await execute(`${clangFormatExecutable} -i decancer.h`, {
-    cwd: join(BINDINGS_DIR, 'native')
-  })
+    await execute(`${clangFormatExecutable} -i decancer.h`, {
+      cwd: join(BINDINGS_DIR, 'native')
+    })
 
-  console.log(`- [${clangFormatExecutable}] completed`)
+    console.log(`- [${clangFormatExecutable}] completed`)
+  }
 }
 
 void (await Promise.all([
-  cargo(join(CORE_DIR)),
-  cargo(join(BINDINGS_DIR, 'java')),
-  cargo(join(BINDINGS_DIR, 'node')),
-  cargo(join(BINDINGS_DIR, 'wasm')),
-  cargo(join(BINDINGS_DIR, 'native')),
+  cargo(join(CORE_DIR), 'core'),
+  cargo(join(BINDINGS_DIR, 'java'), 'java'),
+  cargo(join(BINDINGS_DIR, 'node'), 'node'),
+  cargo(join(BINDINGS_DIR, 'wasm'), 'wasm'),
+  cargo(join(BINDINGS_DIR, 'native'), 'native'),
   clangFormat(),
   prettier(),
   updateReadme()
