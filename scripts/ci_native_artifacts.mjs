56de516cd88892197e99fc43c03e8c73fc0264d7
null8626 <vierofernando9@gmail.com>
2024-06-18 22:03:36 +0700

    fix: try to fix java binding bug attempt 2

--- scripts/ci_native_artifacts.mjs
+++ scripts/ci_native_artifacts.mjs
@@ -7,7 +7,7 @@ import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const TARGET = process.argv[2]
-const IS_JAVA = process.argv.slice(2).some(argv => argv === '--java')
+const IS_JAVA = process.argv.slice(3).some(argv => argv === '--java')
 
 const ROOT_DIR = join(dirname(fileURLToPath(import.meta.url)), '..')
 const TARGET_DIR = join(
@@ -26,7 +26,7 @@ for (let artifact of artifacts) {
     const ext = artifact.match(/\.\w+$/)[0].slice(1)
 
     if (
-      (!IS_JAVA && ext === '.lib') ||
+      (!IS_JAVA && ext === 'lib') ||
       ext === 'dll' ||
       ext === 'so' ||
       ext === 'dylib'
