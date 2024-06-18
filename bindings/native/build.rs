a95f3d426d0c5658faae386d6ffd2154fd167140
null8626 <null8626@users.noreply.github.com>
2024-06-18 14:01:50 +0000

    chore: update files

--- bindings/native/build.rs
+++ bindings/native/build.rs
@@ -1,7 +1,7 @@
 #[cfg(windows)]
 fn embed() {
   println!("cargo:rerun-if-changed=decancer.rc");
-  
+
   let version = env!("CARGO_PKG_VERSION");
   let version_digits = version.split('.').collect::<Vec<_>>();
 
@@ -9,13 +9,16 @@ fn embed() {
   let rc_version_minor = format!("DECANCER_RC_VERSION_MINOR={}", version_digits[1]);
   let rc_version_patch = format!("DECANCER_RC_VERSION_PATCH={}", version_digits[2]);
   let rc_version = format!("DECANCER_RC_VERSION=\"{version}\"");
-  
-  embed_resource::compile("decancer.rc", &[
-    &rc_version_major,
-    &rc_version_minor,
-    &rc_version_patch,
-    &rc_version
-  ])
+
+  embed_resource::compile(
+    "decancer.rc",
+    &[
+      &rc_version_major,
+      &rc_version_minor,
+      &rc_version_patch,
+      &rc_version,
+    ],
+  )
 }
 
 fn main() {
