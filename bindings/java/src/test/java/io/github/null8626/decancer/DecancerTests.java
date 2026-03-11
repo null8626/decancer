494281645aec235ea9e8fb27e2dda34bc223ddc4
null <60427892+null8626@users.noreply.github.com>
2026-03-11 21:32:04 +0700

    style: rename DecancerTest.java to DecancerTests.java [skip ci]

--- bindings/java/src/test/java/io/github/null8626/decancer/DecancerTests.java
+++ bindings/java/src/test/java/io/github/null8626/decancer/DecancerTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class DecancerTest {
+public class DecancerTests {
   private static CuredString CURED = null;
 
   @BeforeAll
