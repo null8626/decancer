01e2bd15e7c420f54deb689efbac7688c5221d76
null <60427892+null8626@users.noreply.github.com>
2026-03-19 00:02:53 +0700

    doc: improve and fix javadoc

---
+++ bindings/java/src/main/java/io/github/null8626/decancer/package-info.java
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2021-2026 null8626
+
+/**
+ * A library that removes common unicode confusables/homoglyphs from strings.
+ *
+ * @author null8626
+ * @version 3.3.3
+ * @since 3.0.1
+ */
+package io.github.null8626.decancer;
